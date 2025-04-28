48a1972b40f89fba703287bb2e8279204175d8b9
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-04-28 14:15:08 -0700

    Slip files

---
+++ firmware/chipignite/polysat/include/slip.h
@@ -0,0 +1,8 @@
+/**
+ * @file slip.h
+ * @brief Serial Line Internet Protocol (SLIP) implementation header
+ *
+ * Provides functions to encode and decode SLIP packets.
+ */
+
+#pragma once
---
+++ firmware/chipignite/polysat/src/slip.c
@@ -0,0 +1,6 @@
+/**
+ * @file slip.c
+ * @brief Serial Line Internet Protocol (SLIP) implementation source
+ *
+ * Provides functions to encode and decode SLIP packets.
+ */
